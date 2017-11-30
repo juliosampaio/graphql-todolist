3d5166443b90781c9bdd050637a95701a7198f24
Julio Sampaio <juliodssampaio@gmail.com>
2017-11-30 18:04:24 -0200

    Add user fields resolvers

--- todolist-graphql-server/resolvers.js
+++ todolist-graphql-server/resolvers.js
@@ -10,5 +10,17 @@ module.exports = {
                 path: `/users/${data.id}`
             });
         }
+    },
+    User: {
+        birthDate: (root) => new Date(root.dob * 1000),
+        todos: (root) => {
+            return request({
+                host: '127.0.0.1',
+                port: 3000,
+                method: 'GET',
+                path: `/users/${root.id}/todos`
+            });
+        },
+        lastName: (root) => root.last_name
     }
 };
